9ebf079ee3c77181e0a80510da4b559a28650769
Jordan Roberts (Jay) <89045532+JayDanielsss@users.noreply.github.com>
2024-08-07 18:43:03 -0400

    Add files via upload
    Tweaked the complexity of the model

--- Jay/hit_array_model.py
+++ Jay/hit_array_model.py
@@ -10,11 +10,11 @@ from create_hitmatrix_and_labels import read_root_file
 import uproot
 print("Preping model")
 root_file = "rootfiles/DY_Target_500k_080524/merged_trackQA_v2.root"
-hitmatrix, Truth_valves, good_events= read_root_file(root_file)
+hitmatrix, Truth_values_elemID,Truth_value_drift, good_events  = read_root_file(root_file)
 
 
 #X is features and Y is labels. Train is for fitting and test is validation/ evaluation
-X_train, X_test, y_train, y_test = train_test_split(hitmatrix, Truth_valves, test_size=0.25, random_state=42, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(hitmatrix, Truth_values_elemID, test_size=0.25, random_state=42, shuffle=True)
 
 checkpoint_filepath = 'checkpoint'
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -29,10 +29,10 @@ callback = tf.keras.callbacks.EarlyStopping(monitor='loss',patience=10,mode='min
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(49,201)))
 model.add(tf.keras.layers.Dropout(0.2))
-model.add(tf.keras.layers.Dense(4925,activation='relu'))
+model.add(tf.keras.layers.Dense(512,activation='relu'))
 model.add(tf.keras.layers.Dropout(0.2))
-model.add(tf.keras.layers.Dense(392,activation='sigmoid'))
-model.add(tf.keras.layers.Dense(196))
+model.add(tf.keras.layers.Dense(256,activation='sigmoid'))
+model.add(tf.keras.layers.Dense(98))
 
 model.summary()
 
@@ -41,7 +41,7 @@ loss_function = tf.keras.losses.MeanSquaredError(reduction='sum_over_batch_size'
 meteric_function = tf.keras.metrics.MeanSquaredError(name='mean_squared_error', dtype=None)
 
 model.compile(loss=loss_function, optimizer=opt, metrics=[meteric_function])
-hist = model.fit(X_train, y_train, epochs=5000, batch_size=16,validation_data=(X_test,y_test),callbacks=[callback])
+hist = model.fit(X_train, y_train, epochs=50, batch_size=32,validation_data=(X_test,y_test),callbacks=[callback])
 
 model.save('hit_array_model')
 
@@ -57,18 +57,21 @@ plt.title("valadation loss vs model loss")
 plt.legend()
 plt.savefig('Hit_array_loss_plot.png')
 
-hit_arrays = model.predict(X_test)
-number_of_events = len(X_test)
 
-hit_arrays = np.reshape(hit_arrays,(number_of_events,49,4))
-test = np.reshape(y_test,(number_of_events,49,4))
-Model_Track_1_elemID = hit_arrays[:,:,0]
-Test_Track_1_elemID = test[:,:,2]
-Model_Track_2_elemID = hit_arrays[:,:,2]
-Test_Track_2_elemID = test[:,:,2]
 
-plt.clf()
-plt.plot(Model_Track_1_elemID[0],color = 'r')
-plt.plot(Test_Track_1_elemID[0])
+# hit_arrays = model.predict(X_test)
+# number_of_events = len(X_test)
 
-plt.savefig('Error_plot.png')
+# hit_arrays = np.reshape(hit_arrays,(number_of_events,49,2))
+# test = np.reshape(y_test,(number_of_events,49,2))
+# print(hit_arrays[0][:,0].astype(int))
+
+# Model_Track_1_elemID = hit_arrays[0][:,0]
+# Test_Track_1_elemID = test[0][:,0]
+# Model_Track_2_elemID = hit_arrays[:,0]
+# Test_Track_2_elemID = test[:,0]
+# plt.clf()
+# plt.plot(Model_Track_1_elemID,color = 'r')
+# plt.plot(Test_Track_1_elemID)
+
+# plt.savefig('Error_plot.png')
